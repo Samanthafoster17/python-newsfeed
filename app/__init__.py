ac46e71264533ad9ae0cd1cedc1a129796cc72d0
Samantha <Samanthafoster17@gmail.com>
2021-05-13 22:32:58 -0400

    saving account, posts, comments, and votes in database

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,12 @@ def create_app(test_config=None):
     SECRET_KEY='super_secret_key'
   )
 
+  
   @app.route('/hello')
   def hello():
     return 'hello world'
 
- # register routes
+# register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
-
   return app
